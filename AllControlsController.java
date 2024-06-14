3805c02a5eab34fa71d263a6157ed0e37e65b1cc
GuerreroEdison <joelguerrero99@outlook.com>
2024-06-14 17:03:53 -0500

    Create AllControlsController.java

---
+++ AllControlsController.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author guerrero
+ */
+package javafxcontrols;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+public class AllControlsController {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private Hyperlink hyperlink;
+
+    @FXML
+    private ToggleButton toggleButton;
+
+    @FXML
+    private RadioButton radioButton;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private ProgressIndicator progressIndicator;
+
+    @FXML
+    private Label progressLabel;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Slider slider;
+
+    // Agrega aqui cualquier metodo que necesites para manejar eventos o logica.
+}
